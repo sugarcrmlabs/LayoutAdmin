96a843743f60ab27edd3c9fad68b11c50af6ef7e
Shad Mickelberry <smickelberry@highlandsolutions.com>
2018-06-15 09:14:40 -0600

    Version 8 compatibility and updated zip. Was not able to load package into Version 8 with package scan on.

--- layout_admin/manifest.php
+++ layout_admin/manifest.php
@@ -15,12 +15,15 @@ $manifest = array (
   'built_in_version' => '7.7.0.0',
   'acceptable_sugar_versions' =>
   array (
-    0 => '',
+    'regex_matches' => array(
+        '7.*','8.*'
+    ),
   ),
   'acceptable_sugar_flavors' =>
   array (
     0 => 'ENT',
     1 => 'ULT',
+    2 => 'PRO',
   ),
   'readme' => '',
   'key' => 'raw',
@@ -28,7 +31,7 @@ $manifest = array (
   'description' => '',
   'icon' => '',
   'is_uninstallable' => false,
-  'name' => 'Layout administration',
+  'name' => 'Layout Administration',
   'published_date' => '2016-11-01 17:43:07',
   'type' => 'module',
   'version' => time(),
